2e05fc6e88158a7686ace1cc74417ee1a3c88e1e
jkvt2 <vince.tan.jun.kai@gmail.com>
2020-10-19 10:58:00 +0800

    Add label mask to main loop

--- human_train_ssd.py
+++ human_train_ssd.py
@@ -91,14 +91,15 @@ def train(loader, net, criterion, optimizer, device, debug_steps, epoch=-1):
     running_regression_loss = 0.0
     running_classification_loss = 0.0
     for i, data in enumerate(loader):
-        images, boxes, labels = data
+        images, boxes, labels, label_mask = data
         images = images.to(device)
         boxes = boxes.to(device)
         labels = labels.to(device)
 
         optimizer.zero_grad()
         confidence, locations = net(images)
-        regression_loss, classification_loss = criterion(confidence, locations, labels, boxes)  # TODO CHANGE BOXES
+        regression_loss, classification_loss = criterion(confidence, locations, labels, boxes,
+                                                         label_mask=label_mask)  # TODO CHANGE BOXES
         loss = regression_loss + classification_loss
         loss.backward()
         optimizer.step()
@@ -128,7 +129,7 @@ def test(loader, net, criterion, device):
     running_classification_loss = 0.0
     num = 0
     for _, data in enumerate(loader):
-        images, boxes, labels = data
+        images, boxes, labels, label_mask = data
         images = images.to(device)
         boxes = boxes.to(device)
         labels = labels.to(device)
@@ -136,7 +137,8 @@ def test(loader, net, criterion, device):
 
         with torch.no_grad():
             confidence, locations = net(images)
-            regression_loss, classification_loss = criterion(confidence, locations, labels, boxes)
+            regression_loss, classification_loss = criterion(confidence, locations, labels, boxes,
+                                                             label_mask=label_mask)
             loss = regression_loss + classification_loss
 
         running_loss += loss.item()
