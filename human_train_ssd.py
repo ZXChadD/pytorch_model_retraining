5cfbb07d8e61f2df1811bd0913224aca37ccf162
ZXChadd <zhexianchadd@gmail.com>
2020-10-26 01:14:35 +0800

    Edit human_train_ssd

--- human_train_ssd.py
+++ human_train_ssd.py
@@ -10,6 +10,7 @@ import glob
 import re
 import eval_ssd
 import human_predict
+import ts_predict
 from vision.utils.misc import str2bool, Timer, freeze_net_layers, store_labels
 from vision.ssd.ssd import MatchPrior
 from vision.ssd.mobilenet_v2_ssd_lite import create_mobilenetv2_ssd_lite
@@ -71,8 +72,6 @@ epoch_limit = train_loop.getint("epoch_limit")
 count = train_loop.getint("count")
 
 # Teacher Student Retraining Loop
-current_FAR = 100
-iteration_count = 0
 pretrained_model_path = "../models/mb2-ssd-lite-mp-0_686.pth"
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
@@ -152,165 +151,180 @@ def test(loader, net, criterion, device):
 if __name__ == '__main__':
 
     human_accuracy = 0.2
-
     lowerbound_model = "../new_results/0-Epoch-134-Loss-1.9067794595445906.pth"
-    logging.info(f"Lowerbound detecting....")
-    human_predict.predict(lowerbound_model, iteration_count, human_accuracy)
 
     while True:
+        current_FAR = 100
+        iteration_count = 0
+        logging.info(f"Lowerbound detecting....")
+        human_predict.predict(lowerbound_model, iteration_count, human_accuracy)
 
-        ####### Delete old saved models #######
-        files = glob.glob("../saved_models" + '/*.pth')
-        for file in files:
-            os.remove(file)
+        while True:
 
-        ####### Initialise variables #######
-        all_files = []
-        min_val_loss = train_loop.getint("min_val_loss")
-        timer = Timer()
+            ####### Delete old saved models #######
+            files = glob.glob("../saved_models" + '/*.pth')
+            for file in files:
+                os.remove(file)
 
-        create_net = lambda num: create_mobilenetv2_ssd_lite(num, width_mult=args.mb2_width_mult)
-        config = mobilenetv1_ssd_config
+            ####### Initialise variables #######
+            all_files = []
+            min_val_loss = train_loop.getint("min_val_loss")
 
-        ####### Augmentation #######
-        train_transform = TrainAugmentation(config.image_size, config.image_mean, config.image_std)
-        target_transform = MatchPrior(config.priors, config.center_variance,
-                                      config.size_variance, 0.5)
+            timer = Timer()
+            create_net = lambda num: create_mobilenetv2_ssd_lite(num, width_mult=args.mb2_width_mult)
+            config = mobilenetv1_ssd_config
 
-        test_transform = TestTransform(config.image_size, config.image_mean, config.image_std)
+            ####### Augmentation #######
+            train_transform = TrainAugmentation(config.image_size, config.image_mean, config.image_std)
+            target_transform = MatchPrior(config.priors, config.center_variance,
+                                          config.size_variance, 0.5)
 
-        ####### Training dataset #######
-        logging.info("Prepare training datasets.")
-        datasets = []
-        for dataset_path in args.datasets:
-            if dataset_path == "../data/expert":
-                dataset = VOCDataset(-1, dataset_path, transform=train_transform,
-                                     target_transform=target_transform)
-            else:
-                dataset = VOCDataset(iteration_count, dataset_path, transform=train_transform,
-                                     target_transform=target_transform)
-            label_file = os.path.join("../models", "voc-model-labels.txt")
-            store_labels(label_file, dataset.class_names)
-            num_classes = len(dataset.class_names)
-            datasets.append(dataset)
+            test_transform = TestTransform(config.image_size, config.image_mean, config.image_std)
 
-        logging.info(f"Stored labels into file {label_file}.")
-        train_dataset = ConcatDataset(datasets)
-        logging.info("Train dataset size: {}".format(len(train_dataset)))
-        train_loader = DataLoader(train_dataset, training.getint("batch_size"),
-                                  num_workers=training.getint("num_workers"),
-                                  shuffle=True)
-
-        ####### Validation dataset #######
-        logging.info("Prepare Validation datasets.")
-        val_dataset = VOCDataset(-1, args.validation_dataset, transform=test_transform,
-                                 target_transform=target_transform, is_test=True)
-        logging.info(val_dataset)
-        logging.info("validation dataset size: {}".format(len(val_dataset)))
-
-        val_loader = DataLoader(val_dataset, training.getint("batch_size"),
-                                num_workers=training.getint("num_workers"),
-                                shuffle=False)
-        logging.info("Build network.")
-        net = create_net(num_classes)
-        min_loss = -10000.0
-        last_epoch = -1
-
-        base_net_lr = args.base_net_lr if args.base_net_lr is not None else args.lr
-        extra_layers_lr = args.extra_layers_lr if args.extra_layers_lr is not None else args.lr
-
-        params = [
-            {'params': net.base_net.parameters(), 'lr': base_net_lr},
-            {'params': itertools.chain(
-                net.source_layer_add_ons.parameters(),
-                net.extras.parameters()
-            ), 'lr': extra_layers_lr},
-            {'params': itertools.chain(
-                net.regression_headers.parameters(),
-                net.classification_headers.parameters()
-            )}
-        ]
-
-        timer.start("Load Model")
-        logging.info(f"Init from pretrained ssd {pretrained_model_path}")
-        net.init_from_pretrained_ssd(pretrained_model_path)
-        logging.info(f'Took {timer.end("Load Model"):.2f} seconds to load the model.')
-
-        net.to(DEVICE)
-
-        criterion = MultiboxLoss(config.priors, iou_threshold=0.5, neg_pos_ratio=3,
-                                 center_variance=0.1, size_variance=0.2, device=DEVICE)
-        optimizer = torch.optim.Adam(params, lr=args.lr)
-
-        logging.info(f"Learning rate: {args.lr}, Base net learning rate: {base_net_lr}, "
-                     + f"Extra Layers learning rate: {extra_layers_lr}.")
-
-        logging.info("Uses CosineAnnealingLR scheduler.")
-        scheduler = CosineAnnealingLR(optimizer, 120, last_epoch=last_epoch)
-        # print(sgd.getfloat("t_max"))
-
-        ####### Training Process #######
-        logging.info(f"Start training from epoch {last_epoch + 1}.")
-        for epoch in range(last_epoch + 1, train_loop.getint("num_epochs")):
-            print("")
-            train(train_loader, net, criterion, optimizer,
-                  device=DEVICE, debug_steps=train_loop.getint("debug_steps"), epoch=epoch)
-            scheduler.step()
-
-            if epoch % train_loop.getint("validation_epochs") == 0 or epoch == train_loop.getint("num_epochs") - 1:
-                val_loss, val_regression_loss, val_classification_loss = test(val_loader, net, criterion,
-                                                                              DEVICE)
-                logging.info(
-                    f"Epoch: {epoch}, " +
-                    f"Validation Loss: {val_loss:.4f}, " +
-                    f"Validation Regression Loss {val_regression_loss:.4f}, " +
-                    f"Validation Classification Loss: {val_classification_loss:.4f}"
-                )
-                model_path = os.path.join("../saved_models", f"{iteration_count}-Epoch-{epoch}-Loss-{val_loss}.pth")
-                net.save(model_path)
-                logging.info(f"Saved model {model_path}")
-
-                if val_loss < min_val_loss:
-                    # Saving the model
-                    if min_val_loss > val_loss:
-                        min_val_loss = val_loss
-                        logging.info('Min loss %0.2f' % min_val_loss)
-                        count = 0
+            ####### Training dataset #######
+            logging.info("Prepare training datasets.")
+            datasets = []
+            print(args.datasets)
+            for dataset_path in args.datasets:
+                if dataset_path == "../data/expert":
+                    dataset = VOCDataset(-1, dataset_path, transform=train_transform,
+                                         target_transform=target_transform)
                 else:
-                    if count == epoch_limit:
-                        # Check early stopping condition
-                        logging.info('Early stopping!')
-                        break
+                    dataset = VOCDataset(iteration_count, dataset_path, transform=train_transform,
+                                         target_transform=target_transform)
+                label_file = os.path.join("../models", "voc-model-labels.txt")
+                store_labels(label_file, dataset.class_names)
+                num_classes = len(dataset.class_names)
+
+                datasets.append(dataset)
+            logging.info(f"Stored labels into file {label_file}.")
+            train_dataset = ConcatDataset(datasets)
+            logging.info("Train dataset size: {}".format(len(train_dataset)))
+            train_loader = DataLoader(train_dataset, training.getint("batch_size"),
+                                      num_workers=training.getint("num_workers"),
+                                      shuffle=True)
+
+            ####### Validation dataset #######
+            logging.info("Prepare Validation datasets.")
+            val_dataset = VOCDataset(-1, args.validation_dataset, transform=test_transform,
+                                     target_transform=target_transform, is_test=True)
+            logging.info(val_dataset)
+            logging.info("validation dataset size: {}".format(len(val_dataset)))
+
+            val_loader = DataLoader(val_dataset, training.getint("batch_size"),
+                                    num_workers=training.getint("num_workers"),
+                                    shuffle=False)
+            logging.info("Build network.")
+            net = create_net(num_classes)
+            min_loss = -10000.0
+            last_epoch = -1
+
+            base_net_lr = args.base_net_lr if args.base_net_lr is not None else args.lr
+            extra_layers_lr = args.extra_layers_lr if args.extra_layers_lr is not None else args.lr
+
+            params = [
+                {'params': net.base_net.parameters(), 'lr': base_net_lr},
+                {'params': itertools.chain(
+                    net.source_layer_add_ons.parameters(),
+                    net.extras.parameters()
+                ), 'lr': extra_layers_lr},
+                {'params': itertools.chain(
+                    net.regression_headers.parameters(),
+                    net.classification_headers.parameters()
+                )}
+            ]
+
+            timer.start("Load Model")
+            logging.info(f"Init from pretrained ssd {pretrained_model_path}")
+            net.init_from_pretrained_ssd(pretrained_model_path)
+            logging.info(f'Took {timer.end("Load Model"):.2f} seconds to load the model.')
+
+            net.to(DEVICE)
+
+            criterion = MultiboxLoss(config.priors, iou_threshold=0.5, neg_pos_ratio=3,
+                                     center_variance=0.1, size_variance=0.2, device=DEVICE)
+            optimizer = torch.optim.Adam(params, lr=args.lr)
+
+            # optimizer = torch.optim.SGD(params, lr=args.lr, momentum=sgd.getfloat("momentum"),
+            #                             weight_decay=sgd.getfloat("weight_decay"))
+
+            logging.info(f"Learning rate: {args.lr}, Base net learning rate: {base_net_lr}, "
+                         + f"Extra Layers learning rate: {extra_layers_lr}.")
+
+            logging.info("Uses CosineAnnealingLR scheduler.")
+            scheduler = CosineAnnealingLR(optimizer, 120, last_epoch=last_epoch)
+            # print(sgd.getfloat("t_max"))
+
+            ####### Training Process #######
+            logging.info(f"Start training from epoch {last_epoch + 1}.")
+            for epoch in range(last_epoch + 1, train_loop.getint("num_epochs")):
+                print("")
+                train(train_loader, net, criterion, optimizer,
+                      device=DEVICE, debug_steps=train_loop.getint("debug_steps"), epoch=epoch)
+                scheduler.step()
+
+                if epoch % train_loop.getint("validation_epochs") == 0 or epoch == train_loop.getint("num_epochs") - 1:
+                    val_loss, val_regression_loss, val_classification_loss = test(val_loader, net, criterion,
+                                                                                  DEVICE)
+                    logging.info(
+                        f"Epoch: {epoch}, " +
+                        f"Validation Loss: {val_loss:.4f}, " +
+                        f"Validation Regression Loss {val_regression_loss:.4f}, " +
+                        f"Validation Classification Loss: {val_classification_loss:.4f}"
+                    )
+                    model_path = os.path.join("../saved_models", f"{iteration_count}-Epoch-{epoch}-Loss-{val_loss}.pth")
+                    net.save(model_path)
+                    logging.info(f"Saved model {model_path}")
+
+                    if val_loss < min_val_loss:
+                        # Saving the model
+                        if min_val_loss > val_loss:
+                            min_val_loss = val_loss
+                            logging.info('Min loss %0.2f' % min_val_loss)
+                            count = 0
                     else:
-                        count += 1
+                        if count == epoch_limit:
+                            # Check early stopping condition
+                            logging.info('Early stopping!')
+                            break
+                        else:
+                            count += 1
 
-        ####### Evaluating Process #######
-        files = glob.glob("../saved_models" + '/*.pth')
-        for file in files:
-            current_file = re.findall('[^-]+(?=.pth)', file)
-            all_files.append(float(current_file[0]))
+            ####### Evaluating Process #######
+            files = glob.glob("../saved_models" + '/*.pth')
+            for file in files:
+                current_file = re.findall('[^-]+(?=.pth)', file)
+                all_files.append(float(current_file[0]))
 
-        min_file_name = min(all_files)
+            min_file_name = min(all_files)
 
-        for x in files:
-            if str(min_file_name) in x:
-                min_file = x
+            for x in files:
+                if str(min_file_name) in x:
+                    min_file = x
 
-        logging.info(f"Evaluating model {min_file}.")
-        new_FAR = eval_ssd.evaluate_ssd(min_file)
-        logging.info(f"Copying file {min_file}.")
-        # copyfile(min_file, "../models")
+            logging.info(f"Evaluating model {min_file}.")
+            new_FAR = eval_ssd.evaluate_ssd(min_file)
+            logging.info(f"Copying file {min_file}.")
+            # copyfile(min_file, "../models")
 
-        if new_FAR < current_FAR:
-            current_FAR = new_FAR
-            iteration_count += 1
-            human_accuracy += 0.05
-            human_predict.predict(lowerbound_model, iteration_count, human_accuracy)
-            logging.info(f"Iteration number: {iteration_count}.")
-            logging.info(f"Clearing cache....")
-            torch.cuda.empty_cache()
+            if new_FAR < current_FAR:
+                current_FAR = new_FAR
+                iteration_count += 1
+                new_model_path = min_file
+                ts_predict.predict(new_model_path, iteration_count)
+                logging.info(f"Iteration number: {iteration_count}.")
+                logging.info(f"Clearing cache....")
+                torch.cuda.empty_cache()
 
-        elif human_accuracy == 0.5:
-            logging.info(f"Human Accuracy: {human_accuracy}")
+            elif current_FAR < new_FAR:
+                human_accuracy += 0.05
+                logging.info(f"Human accuracy: {human_accuracy}.")
+                logging.info(f"Iteration number: {iteration_count}.")
+                logging.info(f"Clearing cache....")
+                torch.cuda.empty_cache()
+                break
+
+        if current_FAR < 8:
             logging.info(f"Training ends")
             break
+
