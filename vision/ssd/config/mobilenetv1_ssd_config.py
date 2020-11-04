d50ca89d2d82bfe5612609a418affbc56e4b8413
ZXChadd <zhexianchadd@gmail.com>
2020-11-04 09:17:38 +0800

    Changed image size to 512

--- vision/ssd/config/mobilenetv1_ssd_config.py
+++ vision/ssd/config/mobilenetv1_ssd_config.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from vision.utils.box_utils import SSDSpec, SSDBoxSizes, generate_ssd_priors
 
-image_size = 600
+image_size = 512
 image_mean = np.array([127, 127, 127]) # RGB layout
 image_std = 128.0
 iou_threshold = 0.45
@@ -15,7 +15,7 @@ specs = [
     SSDSpec(8, 64, SSDBoxSizes(50, 65), [2, 3]),
     SSDSpec(4, 100, SSDBoxSizes(195, 240), [2, 3]),
     SSDSpec(2, 150, SSDBoxSizes(240, 285), [2, 3]),
-    SSDSpec(1, 300, SSDBoxSizes(285, 600), [2, 3])
+    SSDSpec(1, 300, SSDBoxSizes(285, 512), [2, 3])
 ]
 
 
