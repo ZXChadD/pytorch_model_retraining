2b1b959e53e7c68404b17cad2cc9b02c2810d9b1
ZXChadd <zhexianchadd@gmail.com>
2020-11-04 09:03:23 +0800

    Changed commit

--- vision/ssd/config/mobilenetv1_ssd_config.py
+++ vision/ssd/config/mobilenetv1_ssd_config.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from vision.utils.box_utils import SSDSpec, SSDBoxSizes, generate_ssd_priors
 
-image_size = 800
+image_size = 512
 image_mean = np.array([127, 127, 127]) # RGB layout
 image_std = 128.0
 iou_threshold = 0.45
@@ -10,12 +10,12 @@ center_variance = 0.1
 size_variance = 0.2
 
 specs = [
-    SSDSpec(32, 16, SSDBoxSizes(20, 50), [2, 3]),
-    SSDSpec(16, 32, SSDBoxSizes(50, 65), [2, 3]),
-    SSDSpec(8, 64, SSDBoxSizes(65, 240), [2, 3]),
-    SSDSpec(4, 100, SSDBoxSizes(240, 285), [2, 3]),
-    SSDSpec(2, 150, SSDBoxSizes(285, 512), [2, 3]),
-    SSDSpec(1, 300, SSDBoxSizes(512, 800), [2, 3])
+    SSDSpec(32, 16, SSDBoxSizes(20, 35), [2, 3]),
+    SSDSpec(16, 32, SSDBoxSizes(35, 50), [2, 3]),
+    SSDSpec(8, 64, SSDBoxSizes(50, 65), [2, 3]),
+    SSDSpec(4, 100, SSDBoxSizes(195, 240), [2, 3]),
+    SSDSpec(2, 150, SSDBoxSizes(240, 285), [2, 3]),
+    SSDSpec(1, 300, SSDBoxSizes(285, 512), [2, 3])
 ]
 
 
